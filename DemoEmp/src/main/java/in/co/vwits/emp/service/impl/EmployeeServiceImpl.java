2177059e4e1dc73b4497e6f7e1156ea6bf97e3eb
mohammedkaif1 <mohammed.kaif0209@gmail.com>
2023-11-07 21:04:47 +0530

    first commit

---
+++ DemoEmp/src/main/java/in/co/vwits/emp/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,63 @@
+package in.co.vwits.emp.service.impl;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import in.co.vwits.emp.dao.EmployeeDao;
+import in.co.vwits.emp.dao.impl.EmployeeJDBCImpl;
+import in.co.vwits.emp.model.Employee;
+import in.co.vwits.emp.service.EmployeeService;
+
+
+public class EmployeeServiceImpl implements EmployeeService {
+	private EmployeeDao dao;
+	 //Student s=new Student();
+	//private StudentJDBCDaoImpl 
+	public EmployeeServiceImpl()
+	{
+		dao=new EmployeeJDBCImpl();
+	}
+	public List<Employee> All() {
+		// TODO Auto-generated method stub
+		return dao.All();
+	}
+	
+	
+	public void save(Employee s)
+	{
+		dao.save(s);
+		System.out.println("Inserted");
+		
+	}
+	
+	@Override
+	public void UpdateSalById(int id, double sal) {
+		// TODO Auto-generated method stub
+		dao.UpdateSalById(id,sal);
+	}
+	@Override
+	public void DeleteById(int id) {
+		// TODO Auto-generated method stub
+		dao.DeleteById(id);
+		
+	}
+
+	
+	
+
+
+	@Override
+	public List<Employee> sortBySal() {
+		// TODO Auto-generated method stub
+		return dao.All().stream().sorted(Comparator.comparing(Employee::getSalary)).toList();
+                 
+		
+	}
+	public List<Employee> sortByName()
+	{
+		return dao.All().stream().sorted(Comparator.comparing(Employee::getName)).toList();
+	}
+	
+}
+
